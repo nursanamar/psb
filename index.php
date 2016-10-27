4d501460c3203de5485399a7c46c312e074b4f64
Nursan amar <nursan011@gmail.com>
2016-10-27 12:54:43 +0700

    Home selesai

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 	<title>home</title>
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 	<link rel="stylesheet" type="text/css" href="css/psb.css">
-    <link rel="stylesheet" type="text/css" href="css/silide.css">
-    <script type="text/javascript">
+    <link rel="stylesheet" type="text/css" href="css/timeline.css">
+<!--    <script type="text/javascript">
        $(window).scroll(function() {
                     $(".slideanim").each(function(){
                         var pos = $(this).offset().top;
@@ -18,7 +18,7 @@
                         }
                     });
                 }); 
-    </script>
+    </script>-->
 </head>
 <body>
 	<?php include 'header.php'; ?>
@@ -55,41 +55,122 @@
     	<div class="container da slideanim">
     		<h2>Alur Pendaftarn</h2>
     	</div>
-    	<div class="container dc slideanim">
-    		<div class="col-sm-4 col-lg-4 alur">
-    			<div class="icon">
-    				<img src="image/placeholder.jpg">
-    			</div>
-    			<div>
-    				<p><span class="no">1</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-    			</div>
-    			<div class="divider"></div>
-    		</div>
-    		<div class="col-sm-4 col-lg-4 alur">
-    			<div class="icon">
-    				<img src="image/placeholder.jpg">
-    			</div>
-    			<div>
-    				<p><span class="no">1</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-    			</div>
-    			<div class="divider"></div>
-    		</div>
-    		<div class="col-sm-4 col-lg-4 alur">
-    			<div class="icon">
-    				<img src="image/placeholder.jpg">
-    			</div>
-    			<div>
-    				<p><span class="no">1</span>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-    			</div>
-    			<div class="divider"></div>
-    		</div>
+    	<div class="container dc">
+    		<ul class="timeline">
+                <li>
+                    <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h4 class="timeline-title">Isi formulir</h4>
+                            <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>12/12/12</small></p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Mengisi formulir secara online </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-inverted">
+                    <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h4 class="timeline-title">Verifikasi</h4>
+                            <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>12/12/12</small></p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>verifikasi pandafatarn di sekolah sekaligus pengumpulan berkas </p>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h4 class="timeline-title">Tes</h4>
+                            <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>12/12/12</small></p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Mengikuti tes setelah melakukan verifikasi berupa tes kesehatan,wawancara,mengaji, dan tes tulis </p>
+                        </div>
+                    </div>
+                </li>
+                <li class="timeline-inverted">
+                    <div class="timeline-badge"><i class="glyphicon glyphicon-check"></i></div>
+                    <div class="timeline-panel">
+                        <div class="timeline-heading">
+                            <h4 class="timeline-title">Pengumuman</h4>
+                            <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i>12/12/12</small></p>
+                        </div>
+                        <div class="timeline-body">
+                            <p>Pngumuman CSB yang lulus  </p>
+                        </div>
+                    </div>
+                </li>     
+            </ul>
     	</div>
+    </div>
+    <div class="row e">
+        <div class="container">
+            <h2>Tentang SMK 1 Bulukumba</h2>
+        </div>
+        <div class="container">
+            <div class="col-lg-6 col-md-6">
+                <img src="image/placeholder.jpg">
+            </div>
+            <div class="col-lg-6 col-md-6">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in cu</p>
+            </div>
+        </div>
+    </div>
+    <div class="clearfix" style="min-height: 100px;">
+        
+    </div>
+    <div class="footer">
+        <div class="container">
+            <div class="col-lg-4 col-md-4 afoot">
+                
+                <ul>
+                    <h5>lorem</h5>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-4 afoot">
+                
+                <ul>
+                    <h5>lorem</h5>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+            </div>
+            <div class="col-lg-4 col-md-4 afoot">
+                
+                <ul>
+                    <h5>lorem</h5>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                    <li>lorem</li>
+                </ul>
+            </div>
+        </div>
     </div>
      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/docs.min.js"></script>
-    <script type="text/javascript" src="js/slide.js"></script>
+ <!--   <script type="text/javascript" src="js/slide.js"></script>-->
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="/js/ie10-viewport-bug-workaround.js"></script>
 </body>
