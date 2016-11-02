fa1db7d02aa026b0db2a811af86d42116a936a27
Nursan amar <nursan011@gmail.com>
2016-11-02 16:30:19 +0800

    tambah js

--- bantuan.php
+++ bantuan.php
@@ -74,5 +74,12 @@
 			</div>
 		</div>
 	</div>
+	 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/docs.min.js"></script>
+ <!--   <script type="text/javascript" src="js/slide.js"></script>-->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
 </body>
 </html>
