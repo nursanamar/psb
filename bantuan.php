d4f4635b223a3b423160aa5799e99c8bc8a9c4b5
Nursan amar <nursan011@gmail.com>
2016-11-03 11:28:06 +0800

    ubah konten halaman bantuan.php

--- bantuan.php
+++ bantuan.php
@@ -23,50 +23,51 @@
 		</div>
 		<div class="container">
 			<div class="col-md-12 col-sm-12">
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in re</p>
+				<p>Sebelum melakukan pendaftaran ada baiknya anda mengikuti panduan pendafatarn agar anda tidak gampang
+				 bingung </p>
 				<ul>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Pertama sebelum mengisi formulir anda harus menyiapkan data lengkap tentang diri anda bisa berasal
+							dari akte kelahiran,ijazah,kartu keluarga,dll.
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Setelah itu mulailah mengisi formulir dengan data yang benar
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Setelah mengisi formulir anda akan mendapatkan ID dan password serta informasi tentang
+							berkas-berkas yang akan di kumpul saat verifikasi pendaftaraan,catat atau ingat baik-baik ID dan password yang anda dapatkan
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Jika anda kesulitan mengingat password anda bisa lansung login mengunakan ID dan password yang anda dapat,
+							setelah login anda dapat mengubah password,melihat data anda,dan melihat pengumuman
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Setelah itu mulailah menyiapkan berkas-berkas sesuai dengan informasi yang anda dapat setelah mengisi formulir
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Setelah semua berkas lengkap segerah ke sekolah untuk verifikasi dan pengumpulan berkas,saat melakukan verifikasi jika ditanya ID
+							maka sebutkan ID yang anda dapat sebelumnya,panitia akan mengecek data dan berkas anda kemudian akun anda akan diverifikasi
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
 					<li>
-						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+						<p>Setelah akun anda di verifikasi barulah anda bisa mengikuti tes,ikutilah tes secara berurutan pertama adalah tes kesehatan,kedua
+							tes wawancara,ketiga tes mengaji,dan terakhir tes tulis
+						</p>
+						<img src="image/placeholder.jpg">
+					</li>
+					<li>
+						<p>Setelah mengikuti tes anda tinggal menunggu pegumuman lulus,pengumaman dapat dilihat di dalam akun anda
 						</p>
 						<img src="image/placeholder.jpg">
 					</li>
