0afb416b2b617e58a7e8d6593982c397cb732793
Nursan amar <nursan011@gmail.com>
2016-11-11 18:46:00 +0800

    Tambah fitur lihat nilai

---
+++ dashboard staff/datanilai.php
@@ -0,0 +1,153 @@
+<?php
+	include 'cone.php';
+session_start();
+    if (empty($_SESSION['id'])) {
+        echo "<script type='text/javascript'>window.location='http://localhost/psb'</script>";
+    }
+    $id=$_SESSION['id'];
+    $hlmn="datates";
+ ?>
+ <?php
+	if (isset($_GET['id'])) {
+		$hps=mysqli_query($sambung,"DELETE FROM csba WHERE id_csba='$_GET[id]'")or die(mysql_error());
+		$ortu=mysqli_query($sambung,"DELETE FROM orang_tua WHERE id_csba='$_GET[id]'")or die(mysql_error());
+		$wali=mysqli_query($sambung,"DELETE FROM pilihan_jurusan WHERE id_csba='$_GET[id]'")or die(mysql_error());
+		$jur=mysqli_query($sambung,"DELETE FROM wali WHERE id_csba='$_GET[id]'")or die(mysql_error());
+		if ($hps) {
+			echo "<script type='text/javascript'>
+			alert('Terhapus');
+			window.location('index.php')
+			</script>";
+		} else {
+			# code...
+		}
+
+	}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/sidebar.css">
+	<link rel="stylesheet" type="text/css" href="../css/dashboard.css">
+	<link rel="stylesheet" type="text/css" href="../css/stat.css">
+	<script type="text/javascript">
+		function togel() {
+        	var x=document.getElementById("wrapper");
+        document.getElementById("wrapper").classList.toggle("toggled");
+    };
+    </script>
+</head>
+<body>
+	 <?php include 'header.php'; ?>
+    <div id="wrapper" class="togled">
+    	<div class="container-fluid">
+    		<?php include 'sidebar.php'; ?>
+    		<div id="page-content-wrapper">
+    			<div class="container-fluid">
+    				<div class="row kotak">
+    					<div class="col-lg-12">
+    						<?php
+ 								if (isset($_GET['tmbl'])) {
+ 									$ket=mysql_escape_string($_GET['ket']);
+ 									$kit=mysql_escape_string($_GET['kti']);
+ 									$q="SELECT * FROM csba INNER JOIN (pilihan_jurusan INNER JOIN tes ON pilihan_jurusan.id_csba=tes.id_csba) ON csba.id_csba=tes.id_csba WHERE csba.$kit='$ket'";
+							 	}else{
+ 									$q="SELECT * FROM csba INNER JOIN (pilihan_jurusan INNER JOIN tes ON pilihan_jurusan.id_csba=tes.id_csba) ON csba.id_csba=tes.id_csba ORDER BY csba.id_csba DESC";
+ 								}if (isset($_GET['rst'])) {
+ 									$q="SELECT * FROM csba INNER JOIN (pilihan_jurusan INNER JOIN tes ON pilihan_jurusan.id_csba=tes.id_csba) ON csba.id_csba=tes.id_csba ORDER BY csba.id_csba DESC";
+ 								}
+ 								$abl=mysqli_query($sambung,$q) or die(mysqli_error($sambung));
+  							?>
+							<form action="" method="GET" class="cari">
+								<div class="form-group">
+									<div class="col-md-2">
+										<label for="ket">cari</label>
+									</div>
+									<div class="col-md-3">
+										<input type="text" name="ket" class="form-control">
+									</div>
+								</div>
+								<div class="form-group">
+									<div class="col-md-2">
+										<label for="kti">Berdasarkan</label>
+									</div>
+									<div class="col-md-3">
+										<select name="kti" class="form-control">
+ 											<option value="id_csba">id_csba</option>
+ 											<option value="nama">nama</option>
+ 										</select>
+									</div>
+								</div>
+ 								<input type="submit" name="tmbl" class="btn btn-primary">
+ 								<input type="submit" name="rst" value="reset" class="btn btn-primary">
+ 							</form>
+							<table class="table table-bordered">
+                <thead>
+                  <td>NO</td>
+                  <td>ID csba</td>
+             			<td>Nama</td>
+             			<td>Jurusan</td>
+                  <td>Kesehatan</td>
+                  <td>Wawancara</td>
+             			<td>Mengaji</td>
+                  <td>Tulis</td>
+             		</thead>
+							<?php
+								if ($abl) {
+									$no="";
+									while ($data=mysqli_fetch_array($abl)or die(mysqli_error($sambung))) {
+                    $no=$no+1;
+                    $poli=unserialize($data['kesehatan']);
+                		echo "<tr>
+                      <td>$no</td>
+                			<td>$data[id_csba]</td>
+                			<td>$data[nama]</td>
+                			<td>$data[pertama]</td>
+                			<td>berat : $poli[berat] kg<br>
+                          tinggi : $poli[tinggi] cm <br>
+                          tensi : $poli[tensi] Mmhg <br>
+                          rabun : $poli[rabun]<br>
+                          buta warna : $poli[buta] <br>
+                          riwayat merokok : $poli[merokok]<br>
+                          riwayat penyakit : $poli[penyakit]
+                      </td>
+                			<td>$data[wawancara]</td>
+                			<td>$data[mengaji]</td>
+                			<td>$data[tulis]</td>
+                		</tr>";
+
+									}
+								} else {
+									echo "<h1 style='text-align:center'>Tidak ada hasil</h1>";
+								}
+							?>
+							</table>
+    					</div>
+    				</div>
+    				<div class="row kotak">
+    					<div class="col-sm-12 col-md-12">
+    						<form>
+    							<input type="text" name="id" disabled>
+    							<input type="text" name="nama">
+    							<input type="text" name="nisn">
+    							<input type="text" name="pass">
+    							<input type="submit" name="submit" class="btn btn-lg btn-primary">
+    						</form>
+    					</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <script src="../js/vendor/jquery.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/docs.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
--- dashboard staff/sidebar.php
+++ dashboard staff/sidebar.php
@@ -8,6 +8,7 @@
     $panitia="";
     $guru="";
     $tes="";
+    $datates="";
     switch ($hlmn) {
         case 'stat':
             $stat="active";
@@ -36,6 +37,9 @@
         case 'tes':
             $tes="active";
             break;
+        case 'datates':
+            $datates="active";
+            break;
         default:
             # code...
             break;
@@ -54,13 +58,14 @@
                         <li><a href='index.php' class='$data'><span class='glyphicon glyphicon-list-alt' aria-hidden='true'></span>Tabel data csb</a></li>
                         <li><a href='datapanitia.php' class='$panitia'><span class='glyphicon glyphicon-list-alt' aria-hidden='true'></span>Tabel data panitia</a></li>
                         <li><a href='dataguru.php' class='$guru'><span class='glyphicon glyphicon-list-alt' aria-hidden='true'></span>Tabel data guru</a></li>
+                        <li><a href='datanilai.php' class='$datates'><span class='glyphicon glyphicon-list-alt' aria-hidden='true'></span>Tabel data nilai</a></li>
                         <li><a href='pengumuman.php' class='$notif'><span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>Buat pengumuman</a></li>
                         <li><a href='listpengumuman.php' class='$notiflist'><span class='glyphicon glyphicon-list' aria-hidden='true'></span>List pengumuman</a></li>
                         <li><a href='siswalulus.php' class='$lulus'><span class='glyphicon glyphicon-ok' aria-hidden='true'></span>Siswa lulus</a></li>
                         <li><a href='tes.php' class='$tes'><span class='glyphicon glyphicon-upload' aria-hidden='true'></span>upload nilai tes tulis</a></li>
                         <li><a href='download.php' class='$download'><span class='glyphicon glyphicon-cloud-download' aria-hidden='true'></span>Download data</a></li>
                         <li><a href='logout.php'><span class='glyphicon glyphicon-log-out' aria-hidden='true'></span>Log-out</a></li>
-                        
+
                     </ul>
                 </div>
             </div>";
