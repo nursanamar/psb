9097ce3151b310f823c19f122431ac1eb52c971d
Nursan amar <nursan011@gmail.com>
2017-01-31 06:56:28 +0700

    Ubah link redirect

--- dashboard staff/logout.php
+++ dashboard staff/logout.php
@@ -2,5 +2,5 @@
 	session_start();
 	session_unset();
 	session_destroy();
-	echo "<script type='text/javascript'>window.location='http://nursanamar.esy.es//'</script>";
+	echo "<script type='text/javascript'>window.location='http://nursanamar.esy.es/'</script>";
  ?>
